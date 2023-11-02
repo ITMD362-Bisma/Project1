3eb43abd248fc3a63f9ef9384b4395a9577aca56
bismak02 <bkhan2@hawk.iit.edu>
2023-11-02 14:53:27 -0500

    fixing errors

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const signupForm = document.getElementById("signupForm");
   signupForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    // Add your form submission logic here
-    // Example: You can send the form data to a server using fetch()
+    event.preventDefault(); 
   });
 });
